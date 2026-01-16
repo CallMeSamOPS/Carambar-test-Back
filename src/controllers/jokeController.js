2635ed1e4e81a6a30006950efa6419627e9a5c86
Joseph-Alexandre Samuel <samuelja1@outlook.fr>
2026-01-17 00:19:56 +0100

    Creation du controller

--- src/controllers/jokeController.js
+++ src/controllers/jokeController.js
@@ -1,2 +1,22 @@
 const Joke = require('../models/Joke');
 
+exports.createJoke = async (req, res) => {
+    const joke = await Joke.create(req.body);
+    res.json(joke);
+};
+
+exports.getAllJokes = async (req, res) => {
+    const jokes = await Joke.findAll();
+    res.json(jokes);
+};
+
+exports.getJokeById = async (req, res) => {
+    const joke = await Joke.findByPk(req.params.id);
+    res.json(joke);
+};
+
+exports.getRandomJoke = async (req, res) => {
+    const jokes = await Joke.findAll();
+    const random = jokes[Math.floor(Math.random() * jokes.length)];
+    res.json(random);
+}
