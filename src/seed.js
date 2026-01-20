fc192102cd30a476a104a7a0bdf010a7d43eba85
Joseph-Alexandre Samuel <samuelja1@outlook.fr>
2026-01-20 22:03:28 +0100

    ajout seed pour test en prod

---
+++ src/seed.js
@@ -0,0 +1,28 @@
+const Joke = require('./models/Joke');
+
+async function seedJokes() {
+    const count = await Joke.count();
+
+if (count ==== 0) {
+    await Joke.bulkCreate([
+        {
+            question: 'Pourquoi les dev...',
+            answer: 'Parce que les devs...'
+        },
+
+        {
+            question: 'Pourquoi les profs...',
+            answer: 'Parce que les profs...' 
+        },
+
+        {
+            question: 'Pourquoi les sportifs...',
+            answer: 'Parce que les sportifs...' 
+        }
+    ]);
+
+    console.log("Jokes insered");
+    }
+}
+
+module.exports = seedJokes;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const app = require('./app');
 const sequelize = require('./config/database');
+const seedJokes = require('./seed');
 
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync().then(() => {
+sequelize.sync().then(async () => {
     console.log('Base de donnee connectee');
+    await seedJokes();
 
         app.listen(PORT, () => {
         console.log(`Serveur lance sur le port ${PORT}`);
